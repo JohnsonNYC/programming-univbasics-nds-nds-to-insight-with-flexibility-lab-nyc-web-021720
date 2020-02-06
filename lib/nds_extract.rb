a17010b5d1f8e5be2070809716844ca663b257fd
Johnson Kow <jkow95@gmail.com>
2020-02-06 21:49:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,7 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
-  result = 0 
+  result = []
   index = 0 
   
     while index < movies_collection.length do 
