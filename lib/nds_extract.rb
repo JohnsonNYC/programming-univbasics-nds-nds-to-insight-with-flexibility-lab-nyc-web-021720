8bdcd59a8054a207906bf7363536438a85ba5141
Johnson Kow <jkow95@gmail.com>
2020-02-06 21:50:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,6 +53,7 @@ def movies_with_director_key(name, movies_collection)
   index = 0 
   
     while index < movies_collection.length do 
+      movie_data = movies_collection[i]
       result << movie_with_director_name(name, movie_data)
       i += 1 
   end 
